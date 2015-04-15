91690109235f8d86df43902da7455720b9fb5283
Eli Parra <eli@elzr.com>
2015-04-15 18:44:20 -0500

    integrated kuma feedback

--- publish.rb
+++ publish.rb
@@ -1,4 +1,5 @@
 #!/usr/bin/env ruby
+# encoding: utf-8
 require 'optparse'
 require 'rubygems'
 #require 'htmlentities'
@@ -31,11 +32,15 @@ head = '<html><body>'
 footer = '</body></html>'
 header = ''
 navbar = ''
+redirect = ''
 $pathStar = ''
 $pathEnd = ''
 
 if $OPT[:mode] == 'github-path'
 	$pathStart = '/kumamoto-mx/'
+	$pathEnd = ''
+elsif $OPT[:mode] == 'server'
+	$pathStart = '/'
 	$pathEnd = ''
 elsif $OPT[:mode] == 'server'
 	$pathStart = '/'
@@ -83,29 +88,94 @@ end
 File.open(root+"header.html") do |f|
 	header = readAndEncode( f )
 end
+File.open(root+"redirect.html") do |f|
+	redirect = readAndEncode( f )
+end
+
+
+def readProp(prop, html)
+	out = html.match(/^\s*#{prop}:(.*)$/);
+	out ? out[1].strip : '';
+end
+
+sectionData = {
+	'inteligencia-colectiva'=>['Inteligencia Colectiva', 'Creemos que mientras más personas estén involucradas en una toma de decisión o el desarrollo de una idea, mejor será el resultado.'],
+	'participacion-ciudadana'=>['Participación Ciudadana', 'Las personas deben ser las protagonistas de la democracia.'],
+	'apertura'=>['Apertura', 'Reconocemos en la crítica y la oposición una de las fuentes de construcción política más importantes.'],
+	'innovacion'=>['Innovación', 'Solo apalancándonos de nuevas herramientas e ideas podremos lograr cambios radicales en nuestro sistema político.'],
+	'perspectiva-de-genero'=>['Perspectiva de género', 'Queremos construir una sociedad que deje atrás el discurso heteronormativo patriarcal.'],
+	'derechos-humanos'=>['Derechos Humanos', 'Defenderemos los derechos humanos y lucharemos por extenderlos.'],
+	'transparencia'=>['Transparencia', 'Toda la información del quehacer legislativo debe ser un bien público.'],
+
+	'ocupemos-la-ciudad'=>['Ocupemos la ciudad', 'Ocupar la ciudad es recuperar para las personas el espacio en el que viven. Creemos que el distrito 10 puede ser la vanguardia.'],
+	'ciudad-democratica'=>['Ciudad democrática', 'Los problemas de la democracia se resuelven con más democracia.'],
+	'ciudad-sostenible'=>['Ciudad sostenible', 'Una ciudad sostenible es una ciudad con futuro. La zona metropolitana de Guadalajara es el segundo núcleo urbano más grande de México y es urgente tomar medidas para hacer frente a los enormes retos sociales, económicos y ecológicos que toda mega urbe tiene.'],
+	'ciudad-incluyente'=>['Ciudad incluyente', 'La gran tarea de nuestra generación es construir ciudades donde quepan todas y todos.'],
+	'habitemos-el-gobierno'=>['Habitemos el gobierno', 'Habitar el gobierno es ponerle un alto al gobierno de los cínicos y recuperar las riendas de nuestro futuro. Mostrar todo lo que puede hacer un servidor público que ponga primero los intereses de las personas, esa es mi meta como diputado.'],
+	'gobernar-con-las-personas'=>['Gobernar con las personas', 'Limitar la participación de las personas al voto es absurdo en un mundo con herramientas tan complejas como el nuestro.'],
+	'servicio-publico-de-excelencia'=>['Servicio público de excelencia', 'No basta que un representatne sólo cumpla sus tareas mínimas, hay que exigirle excelencia.'],
+	'contrapeso-al-ejecutivo'=>['Contrapeso al ejecutivo', 'El diputado es el principal encargado de vigilar que el trabajo de las autoridades ejecutivas sea real y acorde a la ley.'],
+
+	'en-campanha'=>['En campaña', 'Queremos reivindicar el quehacer político a través de las formas y los fondos. Estos son mis compromisos desde el primer día de campaña.'],
+	'quien-iba-a-creer'=>['Quien iba a creer', 'A los que no creían y que aún no creen prefiero decirles que <strong>esto ya empezó</strong>, que podemos decir con seguridad que hoy ya está mejor y que va a estar mejor cuando decidan creer y se sumen'],
+	'en-el-congreso'=>['En el congreso', 'Queremos reivindicar el quehacer político a través de las formas y los fondos. Estos son mis compromisos durante mi periodo legislativo.']
+}
 
 pages = ['index', 'principios', 'propuestas', 'compromisos', 'kit', 'splash', 'privacidad']
 
-pages.each do |name|
-	File.open(root+name+".html") do |f|
+pages.each do |page|
+	File.open(root+page+".html") do |f|
 		html = readAndEncode( f )
 		
-		title = html.match(/^\s*title:(.*)$/);
-			title = title ? title[1].strip : '';
 		standalone = html.match(/{{standalone}}/)
-		headTemp = head.gsub(/{{title}}/, title)
+
+		# FRONT MATTER
+		title = readProp('title', html);
+		headTemp = head.gsub(/{{title}}/, title);
+
+		og_url = readProp('og_url', html);
+		headTemp = headTemp.gsub(/{{og_url}}/, og_url);
+
+		og_description = readProp('og_description', html);
+		headTemp = headTemp.gsub(/{{og_description}}/, og_description);
+
+		og_image = readProp('og_image', html);
+		headTemp = headTemp.gsub(/{{og_image}}/, og_image);
+		
+
 		html.gsub!(/---.*?---/m, '') #trim front matter
 
 		html = html.sub(/{{navbar}}/, navbar).sub(/{{header}}/, header)
 
-		unless (File.exists?(publicDir+name) or (name=='index') or (name=='splash'))
-			Dir.mkdir(publicDir+name)
+		unless (File.exists?(publicDir+page) or (page=='index') or (page=='splash'))
+			Dir.mkdir(publicDir+page)
 		end
 
-		publicHtml = (name == 'splash' ? publicDir.gsub(/test\//,'') : publicDir)+(
-				(name == 'index' or name=='splash') ?
+		if(['principios', 'propuestas', 'compromisos'].include?(page))
+			html.scan(/data-menuanchor="([^"]+)"/) do |section| section = section[0]
+				thisSectionData = sectionData[section]
+				if section != 'inicio'
+					sectionDir = publicDir+page+'/'+section
+					unless File.exists?( sectionDir )
+						Dir.mkdir( sectionDir )
+					end
+
+					redirectTemp = redirect.gsub(/{{title}}/, thisSectionData[0]+' | Mis '+page).
+						gsub(/{{og_image}}/, 'img/screenshots/'+page+'-'+section+'.jpg').
+						gsub(/{{og_url}}/, page+'/'+section+'/').
+						gsub(/{{og_description}}/, thisSectionData[1]).
+						gsub(/{{redirectUrl}}/, page+'#'+section).
+						gsub(/{{redirectText}}/, '<h1>Mis '+page+' - '+thisSectionData[0]+'</h1>')
+
+					File.open(sectionDir+'/index.html', "w+").write(redirectTemp)
+				end
+			end
+		end
+
+		publicHtml = (page == 'splash' ? publicDir.gsub(/test\//,'') : publicDir)+(
+				(page == 'index' or page=='splash') ?
 					'index' :
-					(name+'/index')
+					(page+'/index')
 		)+'.html'
 
 		File.open(publicHtml, "w+").write(
@@ -115,6 +185,6 @@ pages.each do |name|
 				html
 			end
 		)
-		puts 'Published: '+name
+		puts 'Published: '+page
 	end
 end
